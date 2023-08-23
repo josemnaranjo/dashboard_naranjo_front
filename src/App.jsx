690423e5f7d08b4a1371224fb6de2a3eb3edb391
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-23 11:23:05 -0400

    chore: instalacion radix e implementacion basica

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Button } from "@radix-ui/themes";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hola Mundo!</h1>
+      <h1 className="text-3xl font-bold underline ">Hola Mundo!</h1>
+      <Button size="4" variant="classic" className="bg-red-900">
+        Botón
+      </Button>
     </>
   );
 }
