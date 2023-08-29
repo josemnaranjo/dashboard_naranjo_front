25a1fee039463beceef8a6dd7daebd354a6e3ca2
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-29 11:56:34 -0400

    chore: estilo personalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    theme: {
+      colors: {
+        "primary-light": "#FCBB14",
+        "primary-middle": "#FFBF1A",
+        "primary-dark": "#FFBF18",
+        "secondary-light": "#6272EE",
+        "secondary-middle": "#374BE5",
+        "secondary-dark": "#2236D6",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        xl: "40px",
+        lg: "28px",
+        base: "14px",
+      },
+    },
     extend: {},
   },
   plugins: [],
