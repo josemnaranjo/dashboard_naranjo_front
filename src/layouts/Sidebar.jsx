3ed3d526d7671305d7aa81d9cb70a02ce18a49e1
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-30 11:05:00 -0400

    chore:instalacion de react icons

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,14 +1,14 @@
+import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
-    <div className="bg-secondary-dark h-screen rounded-r-2xl ">
+    <div className="bg-secondary-dark text-white h-screen rounded-r-2xl grid grid-rows-2 place-content-center place-items-center">
       <div>
         <h1>Subtitulo 1</h1>
+        
+        
       </div>
       <div>
         <h1>Subtitulo 2</h1>
-      </div>
-      <div>
-        <h1>Subtitulo 3</h1>
       </div>
     </div>
   );
