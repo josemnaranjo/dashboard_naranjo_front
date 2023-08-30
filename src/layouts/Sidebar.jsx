60d5fa662184c7518760901d3d4800e04668044e
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-30 12:31:49 -0400

    style: hover effect en sidebar link

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -11,30 +11,39 @@ import {
 } from "react-icons/bs";
 const Sidebar = () => {
   return (
-    <div className="bg-secondary-dark text-white text-base h-screen rounded-r-2xl grid grid-rows-2 place-content-center place-items-center">
-      
-      <div className="grid grid-rows-4 p-4">
-        
-        <Link to="/tyv-general" className="flex justify-center items-center font-bold">
+    <div className="bg-secondary-dark text-white text-base h-screen rounded-r-2xl grid grid-rows-2 place-content-center place-items-center px-4">
+      <div className="grid grid-rows-4">
+        <Link
+          to="/tyv-general"
+          className="flex justify-center items-center font-bold"
+        >
           <BsPeople className="w-14 h-12" />
-          <h1>trabajadores y vehículos</h1>
+          <h1 className="pl-1">trabajadores y vehículos</h1>
         </Link>
-        
-        <Link to="/tyv-trabajadores" className="flex justify-center items-center">
+
+        <Link
+          to="/tyv-trabajadores"
+          className="flex justify-center items-center hover:text-primary-dark hover:drop-shadow-lg "
+        >
           <BsPlusCircle className="w-10 h-4" />
           <h1>agregar trabajadores</h1>
         </Link>
 
-        <Link to="/tyv-licencias" className="flex justify-center items-center ">
+        <Link
+          to="/tyv-licencias"
+          className="flex justify-center items-center hover:text-primary-dark hover:drop-shadow-lg  "
+        >
           <BsCalendarWeek className="w-9 h-4" />
           <h1>licencias médicas</h1>
         </Link>
 
-        <Link to="/tyv-vehiculos" className="flex justify-center items-center">
+        <Link
+          to="/tyv-vehiculos"
+          className="flex justify-center items-center hover:text-primary-dark hover:drop-shadow-lg "
+        >
           <BsTruck className="w-9 h-4" />
           <h1>agregar vehículos</h1>
         </Link>
-
       </div>
 
       <div>
