334b8c79572ae419144f3716b9172b150135e934
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-31 12:24:28 -0400

    style: se agrego logo de empresa

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -7,10 +7,17 @@ import {
   BsArrowLeft,
   BsJournal,
 } from "react-icons/bs";
+import Logoempresa from "../assets/images/LogoEmpresa.png";
+
 const Sidebar = () => {
   return (
     <div className="bg-secondary-dark text-white text-base h-screen rounded-r-2xl drop-shadow-sidebar flex flex-col gap-12">
-      <div className="mt-28 grid grid-rows-4 gap-2">
+      
+      <div>
+        <img src={Logoempresa} alt="logo empresa" className="mt-5 ml-3 w-5/6" />
+      </div>
+
+      <div className="grid grid-rows-4 gap-2">
         <Link to="/tyv-general" className=" text-center font-bold">
           <h1 className="pl-1 hover:underline hover:underline-offset-2">
             trabajadores y vehículos
