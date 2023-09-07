2ab468b368dcb379c9f5aeac2e3b4e3bd089fe22
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-09-07 13:02:49 -0300

    style: validacion basica userform

--- src/components/Userform.jsx
+++ src/components/Userform.jsx
@@ -1,68 +1,110 @@
 import { Button } from "@radix-ui/themes";
 import { Form, Formik, Field } from "formik";
+import * as Yup from "yup";
 
-const Userform = ({handleSubmit}) => {
-    return (
-        <div>
-            <Formik
+const Userform = ({ handleSubmit }) => {
+  const valSchema = Yup.object().shape({
+    name: Yup.string().required("Campo obligatorio"),
+
+    lastName: Yup.string().required("Campo obligatorio"),
+
+    email: Yup.string()
+      .email("Formato de correo incorrecto")
+      .required("Campo obligatorio"),
+
+    password: Yup.string()
+      .required("Campo obligatorio")
+      .min(8, "Tu contraseña debe ser más larga")
+      .equals(
+        [Yup.ref("confirmPassword"), null],
+        "las contraseñas no son iguales"
+      ),
+
+    confirmPassword: Yup.string()
+      .required("Campo obligatorio")
+      .min(8, "Tu contraseña debe ser más larga")
+      .equals(
+        [Yup.ref("confirmPassword"), null],
+        "las contraseñas no son iguales"
+      ),
+  });
+  return (
+    <div>
+      <Formik
         initialValues={{
           name: "",
           lastName: "",
           email: "",
           password: "",
+          confirmPassword: "",
         }}
+        validationSchema={valSchema}
         onSubmit={(values, { resetForm }) => {
           handleSubmit(values);
           resetForm();
         }}
       >
-        <Form className="bg-slate-400 text-white container mx-auto w-96 p-8 rounded-md grid grid-rows-6 gap-3">
-          <h1 className="text-xl font-bold">Formulario de registro</h1>
-          <div className="flex flex-col">
-            <label htmlFor="name">nombre</label>
-            <Field
-              id="name"
-              type="text"
-              name="name"
-              className="w-38 rounded-lg px-2 text-black"
-            />
-          </div>
+        {({ errors, touched }) => (
+          <Form className="bg-slate-400 text-white container mx-auto w-96 p-8 rounded-md grid grid-rows-7 gap-3">
+            <h1 className="text-xl font-bold">Formulario de registro</h1>
+            
+            <div className="flex flex-col">
+              <label htmlFor="name">nombre</label>
+              <Field
+                id="name"
+                type="text"
+                name="name"
+                className="w-38 rounded-lg px-2 text-black"
+              />
+              {errors.name && touched.name ? <p className="text-label mt-2 text-red-500">{errors.name}</p>: null}
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="lastName">apellido</label>
-            <Field
-              id="lastName"
-              type="text"
-              name="lastName"
-              className="w-38 rounded-lg px-2 text-black"
-            />
-          </div>
+            <div className="flex flex-col">
+              <label htmlFor="lastName">apellido</label>
+              <Field
+                id="lastName"
+                type="text"
+                name="lastName"
+                className="w-38 rounded-lg px-2 text-black"
+              />
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="email">email</label>
-            <Field
-              id="email"
-              type="text"
-              name="email"
-              className="w-38 rounded-lg px-2 text-black"
-            />
-          </div>
+            <div className="flex flex-col">
+              <label htmlFor="email">email</label>
+              <Field
+                id="email"
+                type="text"
+                name="email"
+                className="w-38 rounded-lg px-2 text-black"
+              />
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="password">contraseña</label>
-            <Field
-              id="password"
-              type="password"
-              name="password"
-              className="w-38 rounded-lg px-2 text-black"
-            />
-          </div>
+            <div className="flex flex-col">
+              <label htmlFor="password">contraseña</label>
+              <Field
+                id="password"
+                type="password"
+                name="password"
+                className="w-38 rounded-lg px-2 text-black"
+              />
+            </div>
 
-          <Button variant="solid">crear</Button>
-        </Form>
+            <div className="flex flex-col">
+              <label htmlFor="confirmPassword">confirmar contraseña</label>
+              <Field
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                className="w-38 rounded-lg px-2 text-black"
+              />
+            </div>
+
+            <Button variant="solid">crear</Button>
+          </Form>
+        )}
       </Formik>
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Userform;
