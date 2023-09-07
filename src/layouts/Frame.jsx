2f3b128ebd4e6df130246bf88df29c7dfbe42b2f
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-09-07 11:42:59 -0300

    style: color frame

--- src/layouts/Frame.jsx
+++ src/layouts/Frame.jsx
@@ -1,6 +1,6 @@
 const Frame = (props) => {
   return (
-    <div className=" bg-frame to-secondary-dark h-[calc(100vh-3rem)] text-black flex items-center justify-center">
+    <div className=" bg-gradient-to-l from-slate-100 to-frame  h-[calc(100vh-3rem)] text-black flex items-center justify-center">
       {props.children}
     </div>
   );
