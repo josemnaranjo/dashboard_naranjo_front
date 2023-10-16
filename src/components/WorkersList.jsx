4d15225dd556f82b28f27401e1b0b5154fa45b43
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-10-16 11:38:32 -0300

    style: diseño basico worker list

--- src/components/WorkersList.jsx
+++ src/components/WorkersList.jsx
@@ -1,14 +1,63 @@
+import { BsSearch } from "react-icons/bs";
 const WorkersList = () => {
   return (
     <div className="bg-gray-300 rounded">
-      <h1>Barra buscadora</h1>
-      <ul>
-        <li className="flex">
-          <h2>Trabajador Uno</h2>
-          <button>borrar</button>
-          <button>editar</button>
+      <div className="pt-7 flex justify-center items-center gap-2">
+        <input
+          type="text"
+          className="rounded-lg px-3"
+          placeholder="buscar trabajador"
+        />
+        <button className="rounded-full border-2 p-1.5 hover:drop-shadow-xl">
+          <BsSearch className="text-white w-3 h-3" />
+        </button>
+      </div>
+      <ul className="px-5 pb-20 pt-8">
+        <li className="grid grid-cols-3 gap-4 mt-3">
+          <h2 className="py-1">Trabajador Uno</h2>
+          <button className="bg-primary-middle text-white rounded-lg px-3 py-1 hover:bg-primary-dark hover:drop-shadow-md">
+            borrar
+          </button>
+          <button className="bg-secondary-middle text-white rounded-lg px-3 py-1 hover:bg-secondary-dark hover:drop-shadow-md">
+            editar
+          </button>
         </li>
-        
+        <li className="grid grid-cols-3 gap-4 mt-3">
+          <h2 className="py-1">Trabajador Dos</h2>
+          <button className="bg-primary-middle text-white rounded-lg px-3 py-1 hover:bg-primary-dark hover:drop-shadow-md">
+            borrar
+          </button>
+          <button className="bg-secondary-middle text-white rounded-lg px-3 py-1 hover:bg-secondary-dark hover:drop-shadow-md">
+            editar
+          </button>
+        </li>
+        <li className="grid grid-cols-3 gap-4 mt-3">
+          <h2 className="py-1">Trabajador Tres</h2>
+          <button className="bg-primary-middle text-white rounded-lg px-3 py-1 hover:bg-primary-dark hover:drop-shadow-md">
+            borrar
+          </button>
+          <button className="bg-secondary-middle text-white rounded-lg px-3 py-1 hover:bg-secondary-dark hover:drop-shadow-md">
+            editar
+          </button>
+        </li>
+        <li className="grid grid-cols-3 gap-4 mt-3">
+          <h2 className="py-1">Trabajador Cuatro</h2>
+          <button className="bg-primary-middle text-white rounded-lg px-3 py-1 hover:bg-primary-dark hover:drop-shadow-md">
+            borrar
+          </button>
+          <button className="bg-secondary-middle text-white rounded-lg px-3 py-1 hover:bg-secondary-dark hover:drop-shadow-md">
+            editar
+          </button>
+        </li>
+        <li className="grid grid-cols-3 gap-4 mt-3">
+          <h2 className="py-1">Trabajador Cinco</h2>
+          <button className="bg-primary-middle text-white rounded-lg px-3 py-1 hover:bg-primary-dark hover:drop-shadow-md">
+            borrar
+          </button>
+          <button className="bg-secondary-middle text-white rounded-lg px-3 py-1 hover:bg-secondary-dark hover:drop-shadow-md">
+            editar
+          </button>
+        </li>
       </ul>
     </div>
   );
