6b9d200879df532e260c8b073627d589805a1516
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-10-25 10:12:58 -0300

    feat: state workers se actualiza cuando agrego trabajador

--- src/store/workerStore.js
+++ src/store/workerStore.js
@@ -4,51 +4,56 @@ import {
   deleteWorker,
   createWorker,
 } from "../api/workers.services";
+import { devtools } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  workers: [],
-  getWorkersAsync: async () => {
-    try {
-      const response = await getAllWorkers();
-      set({
-        workers: response.data,
-      });
-    } catch (err) {
-      alert(
-        "Error con la base de datos. No se pudo recuperar la información solicitada"
-      );
-    }
-  },
-  deleteWorkerAsync: async (rut) => {
-    try {
-      const response = await deleteWorker(rut);
-      const mensaje = response.data.mensaje;
-      if (mensaje === "Trabajador eliminado de la base de datos") {
-        set((state) => ({
-          ...state,
-          workers: state.workers.filter((worker) => worker.rut !== rut),
-        }));
-      } else {
-        alert("No se pudo borra al trabajador solicitado");
+const useStore = create(
+  devtools((set) => ({
+    workers: [],
+    getWorkersAsync: async () => {
+      try {
+        const response = await getAllWorkers();
+        set({
+          workers: response.data,
+        });
+      } catch (err) {
+        alert(
+          "Error con la base de datos. No se pudo recuperar la información solicitada"
+        );
       }
-    } catch (err) {
-      alert(
-        "Error con la base de datos. No se pudo borra al trabajador solicitado"
-      );
-    }
-  },
-  addWorkerAsync: async (worker) => {
-    try {
-      const response = await createWorker(worker);
-      if (response.data.mensaje === "Trabajador creado exitosamente") {
-        alert("Trabajador creado con éxito");
+    },
+    deleteWorkerAsync: async (rut) => {
+      try {
+        const response = await deleteWorker(rut);
+        const mensaje = response.data.mensaje;
+        if (mensaje === "Trabajador eliminado de la base de datos") {
+          set((state) => ({
+            ...state,
+            workers: state.workers.filter((worker) => worker.rut !== rut),
+          }));
+        } else {
+          alert("No se pudo borra al trabajador solicitado");
+        }
+      } catch (err) {
+        alert(
+          "Error con la base de datos. No se pudo borra al trabajador solicitado"
+        );
       }
-    } catch (err) {
-      alert(
-        "Error con la base de datos. No se pudo crear el trabajador solicitado"
-      );
-    }
-  },
-}));
+    },
+    addWorkerAsync: async (worker) => {
+      try {
+        const response = await createWorker(worker);
+        if (response.data.mensaje === "Trabajador creado exitosamente") {
+          const workerData = response.data.newWorker;
+          set((state) => ({ workers: [...state.workers,  workerData ] }));
+          alert("Trabajador creado con éxito");
+        }
+      } catch (err) {
+        alert(
+          "Error con la base de datos. No se pudo crear el trabajador solicitado"
+        );
+      }
+    },
+  }))
+);
 
 export default useStore;
