eb43e27df61477313343abf54a6323b1d2966922
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-11-08 12:25:05 -0300

    feat:mostrar boton de reset cuando se consulta por trabajador

--- src/components/WorkersList.jsx
+++ src/components/WorkersList.jsx
@@ -12,6 +12,7 @@ const WorkersList = ({
   setReset,
 }) => {
   const [worker, setWorker] = useState();
+  const [showReset, setShowReset] = useState(false);
   return (
     <div className="bg-gray-300 rounded-xl flex flex-col justify-center gap-10">
       <div className="flex justify-center items-center gap-2">
@@ -19,20 +20,30 @@ const WorkersList = ({
           type="text"
           className="rounded-lg px-3 w-80 h-7"
           placeholder="buscar por apellido"
-          onChange={(e) => setWorker(e.target.value)}
+          onChange={(e) => {
+            setWorker(e.target.value);
+          }}
         />
         <button
           className="rounded-full border-2 p-1.5 hover:drop-shadow-xl"
-          onClick={() => setSearchWorker(worker)}
+          onClick={() => {
+            setSearchWorker(worker);
+            setShowReset(true);
+          }}
         >
           <BsSearch className="text-white w-3 h-3" />
         </button>
-        <button
-          className="rounded-full border-2 p-1.5 hover:drop-shadow-xl"
-          onClick={() => setReset(!reset)}
-        >
-          <BsArrowRepeat className="text-white w-3 h-3" />
-        </button>
+        {showReset && (
+          <button
+            className="rounded-full border-2 p-1.5 hover:drop-shadow-xl"
+            onClick={() => {
+              setReset(!reset);
+              setShowReset(false);
+            }}
+          >
+            <BsArrowRepeat className="text-white w-3 h-3" />
+          </button>
+        )}
       </div>
       <PaginatedItems
         workersData={workersData}
