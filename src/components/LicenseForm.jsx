32513c336ef5788183eb0d4cedf32747199bed79
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-12-01 10:29:53 -0300

    style: nuevo diseño formulario de creacion licencias

--- src/components/LicenseForm.jsx
+++ src/components/LicenseForm.jsx
@@ -4,6 +4,7 @@ import DatePicker from "react-datepicker";
 import { formatRut, RutFormat } from "@fdograph/rut-utilities";
 import "react-datepicker/dist/react-datepicker.css";
 import dayjs from "dayjs";
+import { BsXSquare } from "react-icons/bs";
 
 const valSchema = Yup.object().shape({
   starDate: Yup.string().required("Campo obligatorio"),
@@ -31,51 +32,58 @@ const createLicense = (workerData, updateWorkerLicense, setToggleForm) => {
       }}
     >
       {({ errors, touched, setFieldValue, values }) => (
-        <Form className="bg-gray-300 text-white container mx-auto w-96 py-20 h-full rounded-xl flex flex-col justify-center gap-10 items-center border-2 border-secondary-middle">
-          <div className="text-xl">
-            <h1>
-              {workerData.name} {workerData.lastName}
-            </h1>
-            <h1 className="text-center">{formatRut(workerData.rut, RutFormat.DOTS_DASH)}</h1>
+        <Form className="bg-gray-300 text-white container mx-auto h-full w-96 rounded-xl border-2  border-secondary-middle">
+          <div className="mt-14 ml-10">
+            <BsXSquare />
           </div>
-          <div className="flex flex-col h-fit">
-            <label htmlFor="starDate" className="text-white">
-              Fecha de inicio
-            </label>
-            <DatePicker
-              selected={values.starDate}
-              onChange={(date) => setFieldValue("starDate", date)}
-              dateFormat={"dd-MM-yyyy"}
-              minDate={new Date()}
-              placeholderText="seleccione fecha de inicio"
-              className="w-48 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm text-black"
-              popperPlacement="top-end"
-            />
-            {errors.starDate && touched.starDate ? (
-              <p className="text-label  text-red-500">{errors.starDate}</p>
-            ) : null}
+          <div className="flex flex-col justify-center items-center gap-8 py-14">
+            <div className="text-xl">
+              <h1>
+                {workerData.name} {workerData.lastName}
+              </h1>
+              <h1 className="text-center">
+                {formatRut(workerData.rut, RutFormat.DOTS_DASH)}
+              </h1>
+            </div>
+            <div className="flex flex-col h-fit">
+              <label htmlFor="starDate" className="text-white">
+                Fecha de inicio
+              </label>
+              <DatePicker
+                selected={values.starDate}
+                onChange={(date) => setFieldValue("starDate", date)}
+                dateFormat={"dd-MM-yyyy"}
+                minDate={new Date()}
+                placeholderText="seleccione fecha de inicio"
+                className="w-48 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm text-black"
+                popperPlacement="top-end"
+              />
+              {errors.starDate && touched.starDate ? (
+                <p className="text-label  text-red-500">{errors.starDate}</p>
+              ) : null}
+            </div>
+            <div className="flex flex-col h-fit">
+              <label htmlFor="finishDate">Fecha de término</label>
+              <DatePicker
+                selected={values.finishDate}
+                onChange={(date) => setFieldValue("finishDate", date)}
+                dateFormat={"dd-MM-yyyy"}
+                minDate={new Date()}
+                placeholderText="seleccione fecha de termino"
+                className="w-48 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm text-black"
+                popperPlacement="top-end"
+              />
+              {errors.finishDate && touched.finishDate ? (
+                <p className="text-label text-red-500">{errors.finishDate}</p>
+              ) : null}
+            </div>
+            <button
+              type="submit"
+              className="bg-secondary-middle text-white rounded-xl h-8 w-40 hover:bg-secondary-dark hover:drop-shadow-md"
+            >
+              registrar licencia
+            </button>
           </div>
-          <div className="flex flex-col h-fit">
-            <label htmlFor="finishDate">Fecha de término</label>
-            <DatePicker
-              selected={values.finishDate}
-              onChange={(date) => setFieldValue("finishDate", date)}
-              dateFormat={"dd-MM-yyyy"}
-              minDate={new Date()}
-              placeholderText="seleccione fecha de termino"
-              className="w-48 rounded-lg border border-stone-400 bg-white px-2 py-1 text-center text-sm text-black"
-              popperPlacement="top-end"
-            />
-            {errors.finishDate && touched.finishDate ? (
-              <p className="text-label text-red-500">{errors.finishDate}</p>
-            ) : null}
-          </div>
-          <button
-            type="submit"
-            className="bg-secondary-middle text-white rounded-xl h-8 w-40 hover:bg-secondary-dark hover:drop-shadow-md"
-          >
-            registrar licencia
-          </button>
         </Form>
       )}
     </Formik>
