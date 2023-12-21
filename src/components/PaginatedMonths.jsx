5f5f434454a461d7e693becf3c6ee2eccd1d4fb8
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-12-21 11:07:03 -0300

    style: pagina central de informes

---
+++ src/components/PaginatedMonths.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+
+const PaginatedMonths = ({ monthsData, itemsPerPage }) => {
+
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = monthsData.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(monthsData.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % monthsData.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <div>
+      <ul className="flex flex-col gap-5">
+        {currentItems?.map((month) => (
+          <li
+            key={month.key}
+            className="bg-gray-300 text-white rounded-lg border-2 flex justify-around py-5 px-2"
+          >
+            <h2 className="w-24">{month.month}</h2>
+            <button className="bg-primary-middle hover:bg-primary-dark hover:drop-shadow-md rounded-lg px-2 text-lg">ver informe</button>
+            <button className="bg-secondary-middle hover:bg-secondary-dark hover:drop-shadow-md rounded-lg px-2 text-lg">descargar informe</button>
+          </li>
+        ))}
+      </ul>
+      <ReactPaginate
+        nextLabel=">"
+        previousLabel="<"
+        onPageChange={handlePageClick}
+        pageCount={pageCount}
+        renderOnZeroPageCount={null}
+        className="w-96 mx-auto mt-14 flex justify-center gap-4 text-white bg-gray-300 rounded-lg border-2"
+        previousLinkClassName="hover:text-primary-dark"
+        nextLinkClassName="hover:text-primary-dark"
+        activeClassName="text-primary-dark underline"
+      />
+    </div>
+  );
+};
+
+export default PaginatedMonths;
--- src/pages/asistInformeGeneral.jsx
+++ src/pages/asistInformeGeneral.jsx
@@ -1,7 +1,23 @@
+import PaginatedMonths from "../components/PaginatedMonths";
+
 const AsistInformeGeneral = () => {
+  const months = [
+    { key: 1, month: "enero" },
+    { key: 2, month: "febrero" },
+    { key: 3, month: "marzo" },
+    { key: 4, month: "abril" },
+    { key: 5, month: "mayo" },
+    { key: 6, month: "junio" },
+    { key: 7, month: "julio" },
+    { key: 8, month: "agosto" },
+    { key: 9, month: "septiembre" },
+    { key: 10, month: "octubre" },
+    { key: 11, month: "noviembre" },
+    { key: 12, month: "diciembre" },
+  ];
   return (
     <div>
-      <h1>Asistencia informe general</h1>
+      <PaginatedMonths monthsData={months} itemsPerPage={4} />
     </div>
   );
 };
